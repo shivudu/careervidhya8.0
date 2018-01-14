7cbafaea95cc00a34a02eace7c6161b564ce5d9c
shivudu <shivudu.s@gmail.com>
2018-01-14 22:39:43 +0530

    commit on edits

--- src/cv/services/AdminService.java
+++ src/cv/services/AdminService.java
@@ -39,7 +39,7 @@ public class AdminService {
 	
 	private MailSender mailSender;
 	
-	//Validate Admin 123
+	//Validate Admin 123 edits
 	public boolean validateAdmin(String email, String password, HttpServletRequest request)
 	{
 		System.out.println("test admin login");
